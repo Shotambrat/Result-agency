c89649f335a77466cd09a370ab9f9b8f97f83dc7
Ismoil Imomov <imomovismoil911@gmail.com>
2024-06-05 18:35:45 +0500

    Blog text shrifti o'zgardi

--- src/components/Blog/AboutClient.jsx
+++ src/components/Blog/AboutClient.jsx
@@ -7,7 +7,7 @@ export default function AboutClient() {
         <h6 className="pb-4 font-semibold text-md leading-5 xs:text-[24px] lg:text-[28px] xl:text-[32px]">
           Мы не делаем однотипных предложений
         </h6>
-        <p className="font-[abel] md:mt-4 text-[11px] xs:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[21px] font-regular">
+        <p className="font-[abel] md:mt-4 text-[11px] xs:text-[14px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px] font-regular">
           Lorem ipsum dolor sit amet consectetur. Et elit interdum eget maecenas
           molestie in ac quisque. Sit nam congue turpis tellus tellus mattis
           vulputate. Cursus eu auctor nam aliquet quam. Risus feugiat volutpat
