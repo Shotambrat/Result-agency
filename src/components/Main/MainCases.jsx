a3bef1f573a4d3e97e4fe22c2b3ef72c4c4d800b
Ismoil Imomov <imomovismoil911@gmail.com>
2024-06-06 11:15:04 +0500

    merge qilishga tayyorgarlik

--- src/components/Main/MainCases.jsx
+++ src/components/Main/MainCases.jsx
@@ -28,7 +28,6 @@ const MainCases = () => {
     },
   ];
 
-
   return (
     <div className="mt-8 lg:mt-[3rem] mx-[0.625rem] sm:mx-4 md:mx-10 xl:mx-20 2xl:mx-[6.875rem] 3xl:max-w-[1500px] 3xl:mx-auto">
       <h2 className="text-headings-color text-[20px] xs:text-[24px] md:text-[30px] 2xl:text-[56px] text-center md:text-left font-semibold lg:font-bold mb-4">
@@ -37,7 +36,7 @@ const MainCases = () => {
       <div className="grid grid-cols-2 sm:grid-cols-3 p-[0.625rem] sm:p-0 gap-x-[0.625rem] lg:gap-x-[1rem] xl:gap-x-6 gap-y-5">
         {cases1.map((elem, index) => (
           <div key={index} className="">
-            <div className="h-[6.5rem] rounded-[7.39px] lg:rounded-3xl lg:h-56 border-[1px] border-solid border-[#d3d2e4]"></div>
+            <div className="rounded-[7.39px] lg:rounded-3xl h-[6.5rem] sm:h-32 lg:h-56 xl:h-[15rem] 2xl:h-[17rem] 3xl:h-[19rem] border-[1px] border-solid border-[#d3d2e4]"></div>
             <h4 className="text-[#191359] font-medium md:font-semibold text-[20px] md:text-[24px] mt-2">
               {elem.title}
             </h4>
@@ -47,7 +46,7 @@ const MainCases = () => {
           </div>
         ))}
         <div>
-          <div className="h-[6.5rem] rounded-[7.39px] text-[14px] md:text-xl lg:text-2xl lg:rounded-3xl lg:h-56 cursor-pointer border-[1px] border-solid border-[#d3d2e4] flex justify-center items-center">
+          <div className="h-[6.5rem] rounded-[7.39px] text-[14px] md:text-xl lg:text-2xl lg:rounded-3xl lg:h-56 xl:h-[15rem] 2xl:h-[17rem] 3xl:h-[19rem] cursor-pointer border-[1px] border-solid border-[#d3d2e4] flex justify-center items-center">
             Заполнить бриф
           </div>
         </div>
