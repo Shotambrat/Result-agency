53136581b195dd8012763f2f2fee294ea222109e
Molly <dudumikah@gmail.com>
2024-06-12 17:12:52 +0500

    Ne izmenil animatsiyu yakorya

--- src/components/Main/ScrollToTopButton.jsx
+++ src/components/Main/ScrollToTopButton.jsx
@@ -5,7 +5,10 @@ const ScrollToTopButton = () => {
     const scrollToTopButton = document.getElementById("scrollToTopButton");
 
     const handleScroll = () => {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
         scrollToTopButton.style.display = "block";
       } else {
         scrollToTopButton.style.display = "none";
@@ -24,27 +27,27 @@ const ScrollToTopButton = () => {
   };
 
   return (
-<button
-  id="scrollToTopButton"
-  title="Go to top"
-  onClick={scrollToTop}
-  className="hidden fixed bottom-5 right-5 z-50 bg-keys_item-bg text-white p-3 rounded-full shadow-lg hover:bg-button-color flex items-center justify-center w-12 h-12"
->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="3"
-      d="M5 15l7-7 7 7"
-    />
-  </svg>
-</button>
+    <button
+      id="scrollToTopButton"
+      title="Go to top"
+      onClick={scrollToTop}
+      className="fixed hidden transition-all duration-300 bottom-5 right-5 z-50 bg-keys_item-bg text-white p-3 rounded-full shadow-lg hover:bg-button-color items-center justify-center w-12 h-12"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="3"
+          d="M5 15l7-7 7 7"
+        />
+      </svg>
+    </button>
   );
 };
 
