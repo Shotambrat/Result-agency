8fbacb7baf22d93bef6fc09c6e531f07bbca43e3
Andrey <aa7155443@gmail.com>
2024-06-14 11:42:26 +0500

    updated translated.json in 2 cases

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import FooterSocial from "./FooterSocial";
 import logoBack from "../assets/img/logoBackButton.svg";
 import { useTranslation } from "react-i18next";
